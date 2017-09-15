0d97e951ac3b84993a2cdf0742e19679fd5057b9
Marcin Konieczny <marcin_k@coni.pl>
2017-09-15 12:33:51 +0200

    Version 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main (String[] args){
         PowerBank powerBank = new PowerBank();
-            powerBank.batteryCapacity = 20000;
-            powerBank.manufacturer = "Xiaomi";
-            powerBank.color = "Biały";
-            powerBank.indicator = true;
+            powerBank.setBatteryCapacity(2000);
+            powerBank.setManufacturer("Xiaomi");
+            powerBank.setColor("Biały niczym śnieg");
+            powerBank.setIndicator(true);
 
-            System.out.println(powerBank.batteryCapacity);
-            System.out.println(powerBank.manufacturer);
-            System.out.println(powerBank.color);
-            if(powerBank.indicator){
+            System.out.println(powerBank.getBatteryCapacity());
+            System.out.println(powerBank.getManufacturer());
+            System.out.println(powerBank.getColor());
+            if(powerBank.getIndicator()){
                 System.out.println("Power Bank jest pełny");
             }else {System.out.println("Power Bank jest pusty");
             }
--- src/PowerBank.java
+++ src/PowerBank.java
@@ -1,9 +1,38 @@
 public class PowerBank {
-    String manufacturer;
-    int batteryCapacity;
-    String color;
-    boolean indicator; //full empty
+    private String manufacturer;
+    private int batteryCapacity;
+    private String color;
+    private boolean indicator; //full empty
 
+        public PowerBank(){
+            this.manufacturer = manufacturer;
+            this.batteryCapacity = batteryCapacity;
+            this.color = color;
+            this.indicator = indicator;
+        }
 
-
+        void setManufacturer(String manufacturer){
+            this.manufacturer = manufacturer;
+        }
+        String getManufacturer(){
+            return manufacturer;
+        }
+        void setBatteryCapacity(int batteryCapacity){
+            this.batteryCapacity = batteryCapacity;
+        }
+        int getBatteryCapacity(){
+            return batteryCapacity;
+        }
+        void setColor(String color){
+            this.color = color;
+        }
+        String getColor(){
+            return color;
+        }
+        void setIndicator(boolean indicator){
+            this.indicator = indicator;
+        }
+        boolean getIndicator(){
+            return indicator;
+        }
 }
